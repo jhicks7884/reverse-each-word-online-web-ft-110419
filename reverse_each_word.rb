1b2d1e17378e572ca73b084cdcf71b798a4460c1
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-06 21:05:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,6 @@
-reverse_each_word("Hello there, and how are you?")
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
+end
